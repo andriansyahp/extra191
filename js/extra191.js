e6fd333f6fc02ceca35e29e29c0bdc6839d8db07
andriansyahp <muhammad.andriansyah@ui.ac.id>
2019-07-17 22:31:17 +0700

    Added feature auto-scroll to the top of the clicked accordion section

--- js/extra191.js
+++ js/extra191.js
@@ -42,23 +42,34 @@
 
 })(jQuery); // End of use strict
 
+$('.collapse').on('shown.bs.collapse', function (e) {
+  if ($(this).data('noscroll')!=1){
+	var $card = $(this).closest('.card');
+	$('html,body').animate({
+		scrollTop: $card.offset().top
+	}, 500);
+  }
+  // reset noscroll data
+  $(this).data('noscroll',0);
+});
+
 // Disable Google Maps scrolling
 // See http://stackoverflow.com/a/25904582/1607849
 // Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
-  var that = $(this);
-  that.on('click', onMapClickHandler);
-  that.off('mouseleave', onMapMouseleaveHandler);
-  that.find('iframe').css("pointer-events", "none");
-}
-var onMapClickHandler = function(event) {
-  var that = $(this);
-  // Disable the click handler until the user leaves the map area
-  that.off('click', onMapClickHandler);
-  // Enable scrolling zoom
-  that.find('iframe').css("pointer-events", "auto");
-  // Handle the mouse leave event
-  that.on('mouseleave', onMapMouseleaveHandler);
-}
-// Enable map zooming with mouse scroll when the user clicks the map
-$('.map').on('click', onMapClickHandler);
+// var onMapMouseleaveHandler = function(event) {
+//   var that = $(this);
+//   that.on('click', onMapClickHandler);
+//   that.off('mouseleave', onMapMouseleaveHandler);
+//   that.find('iframe').css("pointer-events", "none");
+// }
+// var onMapClickHandler = function(event) {
+//   var that = $(this);
+//   // Disable the click handler until the user leaves the map area
+//   that.off('click', onMapClickHandler);
+//   // Enable scrolling zoom
+//   that.find('iframe').css("pointer-events", "auto");
+//   // Handle the mouse leave event
+//   that.on('mouseleave', onMapMouseleaveHandler);
+// }
+// // Enable map zooming with mouse scroll when the user clicks the map
+// $('.map').on('click', onMapClickHandler);
